21b0a6686c54ba6e177e5e47c125d825bca561c9
guillermoandrae <guillermo.fisher@cloudreach.com>
2019-03-23 21:09:58 -0400

    Parsed the content for links, @s, and code. Regex ftw.

--- src/js/search.js
+++ src/js/search.js
@@ -19,7 +19,14 @@ $(document).ready(function() {
             var link = document.createElement("a");
             link.setAttribute("href", data.htmlUrl);
             link.innerHTML = data.source;
-            $("td:eq(0)", row).html(link);            
+            $("td:eq(0)", row).html(link);
+            var content = "";
+            var urlPrefix = "https://" + data.source.toLowerCase() + ".com";
+            content = data.body.replace(/\@([a-z]+)([ ;:.]?)/ig, '<a href="' + urlPrefix + '/$1">@$1</a>$2');
+            content = content.replace(/http(.*)(jpg|png|gif)/ig, '<a href="$&">$&</a>');
+            content = content.replace(/https:\/\/t.co\/(.*)/ig, '<a href="$&">$&</a>');
+            content = content.replace(/\`(.*)\`/ig, "<code>$1</code>");
+            $("td:eq(1)", row).html(content);                 
             var timestamp = (data.createdAt.length === 0) ? parseFloat(data.id) : parseFloat(data.createdAt) * 1000;
             var newDate = new Date(timestamp);
             $("td:eq(2)", row).html(newDate.toLocaleDateString());
