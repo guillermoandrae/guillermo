e97b1c52867f761474b838dd2781bba9434d9e13
guillermoandrae <guillermo.fisher@cloudreach.com>
2019-03-23 20:01:57 -0400

    Some refactoring

---
+++ src/js/search.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("#posts").DataTable({
+        "info": false,
+        "lengthChange": false,
+        "pageLength": 7,
+        "pagingType": "full_numbers",
+        "order": [[ 2, "desc" ]],
+        "ajax": "https://api.guillermoandraefisher.com/posts?limit=9999",
+        "columns": [
+            { "data": "source" },
+            { "data": "body" },
+            { "data": "id" }
+        ],
+        "language": {
+            "search": "_INPUT_",
+            "searchPlaceholder": "Search..."
+        },
+        "rowCallback": function( row, data, dataIndex ) {
+            var link = document.createElement("a");
+            link.setAttribute("href", data.htmlUrl);
+            link.innerHTML = data.source;
+            $("td:eq(0)", row).html(link);            
+            var timestamp = (data.createdAt.length === 0) ? parseFloat(data.id) : parseFloat(data.createdAt) * 1000;
+            var newDate = new Date(timestamp);
+            $("td:eq(2)", row).html(newDate.toLocaleDateString());
+        }
+    });
+});
