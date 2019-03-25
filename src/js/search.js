8128bdb38377ef9912e5f067c16b388dee55c7e5
guillermoandrae <guillermo.fisher@cloudreach.com>
2019-03-24 21:57:59 -0400

    More URLs in the post body and some CSS changes.

--- src/js/search.js
+++ src/js/search.js
@@ -20,12 +20,26 @@ $(document).ready(function() {
             link.setAttribute("href", data.htmlUrl);
             link.innerHTML = data.source;
             $("td:eq(0)", row).html(link);
-            var content = "";
-            var urlPrefix = "https://" + data.source.toLowerCase() + ".com";
-            content = data.body.replace(/\@([a-z]+)([ ;:.]?)/ig, '<a href="' + urlPrefix + '/$1">@$1</a>$2');
-            content = content.replace(/http(.*)(jpg|png|gif)/ig, '<a href="$&">$&</a>');
-            content = content.replace(/https:\/\/t.co\/(.*)/ig, '<a href="$&">$&</a>');
-            content = content.replace(/\`(.*)\`/ig, "<code>$1</code>");
+            var postUrl = hashTagUrl = "";
+            switch (data.source.toLowerCase()) {
+                case "github":
+                    postUrl = "https://github.com/$1";
+                    hashTagUrl = "https://github.com/guillermoandrae";                    
+                    break;
+                case "instagram":
+                    postUrl = "https://instagram.com/$1";
+                    hashTagUrl = "https://www.instagram.com/explore/tags/$1/";
+                    break;
+                case "twitter":
+                    postUrl = "https://twitter.com/$1";
+                    hashTagUrl = "https://twitter.com/hashtag/$1?src=hash";
+                    break;
+            }
+            var content = data.body.replace(/\@([a-z0-9_]+)([ ;:.]?)/ig, '<a href="' + postUrl + '">@$1</a>$2')
+                        .replace(/http(.*)(jpg|png|gif)/ig, '<a href="$&">$&</a>')
+                        .replace(/https:\/\/t.co\/([a-z0-9]+){10}/ig, '<a href="$&">$&</a>')
+                        .replace(/\`(.*)\`/ig, "<code>$1</code>")
+                        .replace(/\#([a-z0-9_-]+)([ ;:.]?)/ig, '<a href="' + hashTagUrl + '">#$1</a>$2');
             $("td:eq(1)", row).html(content);                 
             var timestamp = (data.createdAt.length === 0) ? parseFloat(data.id) : parseFloat(data.createdAt) * 1000;
             var newDate = new Date(timestamp);
